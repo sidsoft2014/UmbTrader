b02a189dec141f2bf89a704d5e8a695696d08138
sidsoft2014 <sidsoft2014@outlook.com>
2016-07-16 19:58:56 +0100

    initial commit

---
+++ UmbTrader/gulpfile.js
@@ -0,0 +1,38 @@
+﻿var gulp = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    gUtil = require('gulp-util'),
+    nano = require('gulp-cssnano'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    inject = require('gulp-inject'),
+    del = require('del');
+
+var paths = {
+    sass: {
+        src: './sitefiles/styles/sass/**/*.scss',
+        dest: './sitefiles/dist/styles'
+    },
+    js: {
+        src: './sitefiles/scripts/js/**/*.js',
+        dest: './sitefiles/dist/scripts'
+    }
+}
+
+gulp.task('sass', function () {
+    return gulp.src(paths.sass.src)
+        .pipe(sass())
+        .pipe(gulp.dest(paths.sass.dest));        
+});
+
+gulp.task('uglify', function () {
+    return gulp.src(paths.js.src)
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.js.dest));
+});
+
+gulp.task('watch', ['sass', 'uglify'], function () {
+    gulp.watch(paths.sass.src, ['sass']);
+    gulp.waych(paths.js.src, ['uglify']);
+});
+
+gulp.task('default', ['watch']);
